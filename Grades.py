8d5bfba8d0413dd108af016eedfb1b0400d12965
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-08-21 12:28:46 +0530

    Add files via upload

---
+++ Grades.py
@@ -0,0 +1,12 @@
+marks = input("marks : ")
+if(marks >= 90):
+    print("A")
+
+elif(marks >= 80 and marks < 90):
+    print("B")
+
+elif(marks >= 70 and marks < 80):
+    print("C")
+    
+else:
+    print("D")
