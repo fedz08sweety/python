f459b5e05a238b4bbc02a704f85e122c4b1eed15
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-08-21 12:29:21 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+name = input("enter your name:")
+print("Welcome", name)
+
+name = input("enter your age:")
+print("you entered", name)
+
+name = input("enter name:")
+age = int(input("enter your age:"))
+marks = float(input("enter makrs:"))
+
+print("Welcome", name)
+print("age =", age)
+print("marks =", marks)
