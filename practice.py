0c9adc202fb2799981a693a9badf54d2ea6a32b4
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-08-21 12:29:57 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,20 @@
+#que 1
+first = int(input("enter first: "))
+second = int(input("enter second:"))
+
+print("sum =", first + second)
+
+#que 2
+side = float(input("enter square side : "))
+print("area =", side ** 2)
+
+
+#que 3
+a = float(input("enter first :"))
+b = float(input("enter second :"))
+print("avg =", (a+b/2))
+
+#que 4
+a = float(input("enter first :"))
+b = float(input("enter second :"))
+print(a >= b)
