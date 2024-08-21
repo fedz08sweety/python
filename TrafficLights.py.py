412365f0ed9b72475dde3a48ae68b45137209d8e
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-08-21 12:30:35 +0530

    Add files via upload

---
+++ TrafficLights.py.py
@@ -0,0 +1,10 @@
+Light = input("Light color : ")
+if(Light == "red"):
+    print("stop")
+elif(Light == "yellow"):
+    print("look")
+elif(Light == "green"):
+    print("go")
+else:
+    print("Light is broken")
+    
