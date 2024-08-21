5fbc2acf0e72074db4122ecf61a00ede5c9ec18c
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-08-21 12:29:38 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,41 @@
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) #remainder
+print(a ** b) #reaminder a^b
+
+#relational operators
+a = 50
+b = 20
+
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #True
+print(a > b) #True
+print(a <= b) #False
+print(a < b) #False
+
+#assignment operators
+num = 10 
+num += 10
+print("num : ", num)
+
+#logical operators
+a = 50
+b = 30
+print(not False)
+print(not (a < b))
+
+#and operator
+val1 = True
+val2 = False
+print("and operator:", val1 and val2)
+
+#or operator
+print("or operator:", val1 or val2)
+
+print("or operator:", (a == b) or (a>b))
