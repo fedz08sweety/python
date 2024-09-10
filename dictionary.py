0455808038c8e2c28e9e6c033e3889c155291758
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-09-10 20:35:33 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+dict = {'Mon':3,'Tue':5,'Wed':6,'Thu':9}
+
+print("The given dictionary : ",dict)
+
+check_key = input("Enter Key to check: ")
+
+check_value = input("Enter Value: ")
+
+if check_key in dict:
+
+    print(check_key,"is Present.")
+
+    dict.pop(check_key)
+
+    dict[check_key]=check_value
+
+else:
+
+    print(check_key, " is not Present.")
+
+    dict[check_key]=check_value
+
+    print("Updated dictionary : ",dict)
