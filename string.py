869e744362bae6b3463b76f3f1711c845b87bb2c
Sweety Fernandes <156332741+fedz08sweety@users.noreply.github.com>
2024-09-10 20:38:40 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,20 @@
+class MyClass:
+
+                def Get_String(self):
+
+                                self.MyStr=input("Enter any String: ")
+
+                def Print_String(self):
+
+                                s=self.MyStr
+
+                                print("String in Upper Case: " , s.upper())
+
+# main body
+
+Obj=MyClass()
+
+Obj.Get_String()
+
+Obj.Print_String()
+
